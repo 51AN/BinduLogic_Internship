21ba6a0ee2f757c1d55191c08d15e4fd1bb0afb0
51AN <sianashsad@iut-dhaka.edu>
2024-06-09 11:44:19 +0200

    updated server file to read home.html with error handling

--- Project 1.0 - Vanilla Essence/server.js
+++ Project 1.0 - Vanilla Essence/server.js
@@ -1,8 +1,18 @@
 const http = require('http');
+const fs = require('fs');
+const path = require('path');
 
 const server = http.createServer((req, res) => {
     const route = req.url;
-    console.log(route);
+    fs.readFile(path.join(__dirname, 'pages', 'home.html'), (err, data) => {
+        if (err) {
+            res.writeHead(500, { 'Content-Type': 'text/plain' });
+            res.end('500 - Internal Server Error');
+        } else {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(data);
+        }
+    });
     
 });
 
