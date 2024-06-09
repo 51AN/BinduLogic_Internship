0ed9d1fbf34b829f609dc1f1485c6cab875c9986
51AN <sianashsad@iut-dhaka.edu>
2024-06-09 11:48:29 +0200

    updated server.js with helper function to serve file

--- Project 1.0 - Vanilla Essence/server.js
+++ Project 1.0 - Vanilla Essence/server.js
@@ -2,18 +2,23 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
-const server = http.createServer((req, res) => {
-    const route = req.url;
-    fs.readFile(path.join(__dirname, 'pages', 'home.html'), (err, data) => {
+//helper function
+const serveFile = (res, filePath, contentType) => {
+    fs.readFile(filePath, (err, data) => {
         if (err) {
             res.writeHead(500, { 'Content-Type': 'text/plain' });
             res.end('500 - Internal Server Error');
         } else {
-            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.writeHead(200, { 'Content-Type': contentType });
             res.end(data);
         }
     });
-    
+};
+
+// http server
+const server = http.createServer((req, res) => {
+    const route = req.url;
+    serveFile(res, path.join(__dirname, 'pages', 'books.html'), 'text/html');    
 });
 
 const PORT = 3000;
